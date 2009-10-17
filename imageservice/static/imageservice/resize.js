373bf264dc0c319bbd859aada8d85c1bd6c0afc8
Daniel Hasselrot <daniel.hasselrot@gmail.com>
2009-10-17 19:44:12 +0200

    Removed javascript console logging

--- imageservice/static/imageservice/resize.js
+++ imageservice/static/imageservice/resize.js
@@ -3,13 +3,11 @@ resize = function(url, width, height) {
     var filenameStart = Math.max(url.lastIndexOf("/"), 0);
     var path = url.substring(0, filenameStart);
     var filename = url.substring(filenameStart);
-    console.info(filename)
     var extensionStart = filename.lastIndexOf(".");
     if (extensionStart == -1) {
         extensionStart = filename.length;
     }
     var extension = filename.substring(extensionStart);
-    console.info(extension)
     var filenameWithoutExtension = filename.substring(0, extensionStart);
     return path + filenameWithoutExtension + "." + width.toString() + 
            "x" + height.toString() + extension;
